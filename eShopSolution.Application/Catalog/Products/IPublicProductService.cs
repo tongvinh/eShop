2fc206556763c27bdefaf026eb3aca05565ce924
Tống Gia Vĩnh <tonggiavinh@outlook.com>
2020-08-27 10:17:59 +0700

    Add Backend Api, Create Swagger

--- eShopSolution.Application/Catalog/Products/IPublicProductService.cs
+++ eShopSolution.Application/Catalog/Products/IPublicProductService.cs
@@ -1,6 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using eShopSolution.ViewModels.Catalog.Products;
-using eShopSolution.ViewModels.Catalog.Products.Public;
 using eShopSolution.ViewModels.Common;
 
 namespace eShopSolution.Application.Catalog.Products
@@ -8,5 +8,7 @@ namespace eShopSolution.Application.Catalog.Products
     public interface IPublicProductService
     {
         public Task<PagedResult<ProductViewModel>> GetAllByCategoryId(GetPublicProductPagingRequest request);
+
+        Task<List<ProductViewModel>> GetAll();
     }
 }
